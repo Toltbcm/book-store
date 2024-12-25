3695091be80b704cd65773b144c4ce7d3a15a8c5
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-12-25 07:29:45 +0200

    remove redundant property

--- src/main/java/org/bookstore/BookStoreApplication.java
+++ src/main/java/org/bookstore/BookStoreApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = "org.bookstore")
+@SpringBootApplication
 public class BookStoreApplication {
 
     private final BookService bookService;
