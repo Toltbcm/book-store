94cae068b0ace440074070ccf9253700cf183948
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-12-25 08:43:40 +0200

    add BookController

---
+++ src/main/java/org/bookstore/controller/BookController.java
@@ -0,0 +1,38 @@
+package org.bookstore.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.bookstore.dto.BookDto;
+import org.bookstore.dto.CreateBookDto;
+import org.bookstore.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getAll() {
+        return ResponseEntity.ok(bookService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<BookDto> create(@RequestBody CreateBookDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.save(dto));
+    }
+}
