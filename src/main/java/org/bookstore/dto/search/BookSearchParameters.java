6db54a24cc336a056624a12e9dbb3bec61884574
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-02 12:38:59 +0200

    delete redundant annotation

--- src/main/java/org/bookstore/dto/search/BookSearchParameters.java
+++ src/main/java/org/bookstore/dto/search/BookSearchParameters.java
@@ -1,13 +1,11 @@
 package org.bookstore.dto.search;
 
-import jakarta.validation.constraints.DecimalMin;
 import java.math.BigDecimal;
 
 public record BookSearchParameters(
         String titlePart,
         String authorPart,
         String isbnPart,
-        @DecimalMin(value = "0.0")
         BigDecimal minPrice,
         BigDecimal maxPrice,
         String descriptionPart) {
