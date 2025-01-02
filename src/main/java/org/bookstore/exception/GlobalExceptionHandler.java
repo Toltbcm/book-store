408339919ea7b20b00377f8f92b5c65cac10fbed
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-02 15:17:13 +0200

    refactor

--- src/main/java/org/bookstore/exception/GlobalExceptionHandler.java
+++ src/main/java/org/bookstore/exception/GlobalExceptionHandler.java
@@ -15,29 +15,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final String UNEXPECTED_ERROR = "An unexpected error occurred";
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
-
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.BAD_REQUEST);
-
+        Map<String, Object> body = prepareBody(HttpStatus.BAD_REQUEST);
         List<String> errors = ex.getBindingResult()
                 .getAllErrors().stream()
                 .map(this::getErrorMessage)
                 .toList();
-
         body.put("errors", errors);
-
         return ResponseEntity.badRequest().body(body);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllExceptions(Exception ex) {
-        return ResponseEntity.internalServerError().body(UNEXPECTED_ERROR);
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex) {
+        Map<String, Object> body = prepareBody(HttpStatus.INTERNAL_SERVER_ERROR);
+        body.put("error", ex.getMessage());
+        return ResponseEntity.internalServerError().body(body);
+    }
+
+    private Map<String, Object> prepareBody(HttpStatus status) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status);
+        return body;
     }
 
     private String getErrorMessage(ObjectError e) {
