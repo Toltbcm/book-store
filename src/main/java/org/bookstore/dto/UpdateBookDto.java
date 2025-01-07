2eb5e154245f3378759e4906d077c3282be502d6
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-07 16:23:45 +0200

    add some validations to update dto

--- src/main/java/org/bookstore/dto/UpdateBookDto.java
+++ src/main/java/org/bookstore/dto/UpdateBookDto.java
@@ -1,18 +1,24 @@
 package org.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class UpdateBookDto {
 
+    @NotBlank
     private String title;
 
+    @NotBlank
     private String author;
 
+    @NotBlank
     private String isbn;
 
+    @NotNull
     @Min(0)
     private BigDecimal price;
 
