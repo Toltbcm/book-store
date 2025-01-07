70af1759f0a689351dbed7681bee58a3d80c0acd
Anatolii <60557891+Toltbcm@users.noreply.github.com>
2025-01-07 14:33:51 +0200

    Feature/book entity and repo (#1)
    * add properties
    
    * add the book repository
    
    * add findAll out
    
    * edit pom.xml
    
    * remove redundant property
    
    * change table name
    
    * add name for id column
    
    * rename exception

--- src/main/java/org/bookstore/BookStoreApplication.java
+++ src/main/java/org/bookstore/BookStoreApplication.java
@@ -1,13 +1,39 @@
 package org.bookstore;
 
+import java.math.BigDecimal;
+import org.bookstore.model.Book;
+import org.bookstore.service.BookService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+
+    private final BookService bookService;
+
+    public BookStoreApplication(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setTitle("Fairy tales");
+            book.setAuthor("Bob");
+            book.setIsbn("000-0-00-000000-0");
+            book.setPrice(BigDecimal.valueOf(33.55));
+            book.setDescription("Bla bla bla");
+            book.setCoverImage("000-0-00-000000-0.jpg");
+
+            System.out.println("Saved book: " + bookService.save(book));
+            System.out.println("Books from db:" + bookService.findAll());
+        };
+    }
 }
