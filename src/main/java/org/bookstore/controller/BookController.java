4cbddbd1026237f78de4af6ad5a4f153b61243bd
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-07 15:09:06 +0200

    change response status

--- src/main/java/org/bookstore/controller/BookController.java
+++ src/main/java/org/bookstore/controller/BookController.java
@@ -7,12 +7,12 @@ import org.bookstore.dto.BookDto;
 import org.bookstore.dto.CreateBookDto;
 import org.bookstore.service.BookService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,17 +23,18 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<BookDto> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(bookService.findById(id));
+    public BookDto getById(@PathVariable Long id) {
+        return bookService.findById(id);
     }
 
     @GetMapping
-    public ResponseEntity<List<BookDto>> getAll() {
-        return ResponseEntity.ok(bookService.findAll());
+    public List<BookDto> getAll() {
+        return bookService.findAll();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<BookDto> create(@Valid @RequestBody CreateBookDto requestDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.save(requestDto));
+    public BookDto create(@Valid @RequestBody CreateBookDto requestDto) {
+        return bookService.save(requestDto);
     }
 }
