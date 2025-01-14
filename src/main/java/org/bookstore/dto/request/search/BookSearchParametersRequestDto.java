c6296f518ca2cd32079eb00a59fcef450a2d078f
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-14 15:10:06 +0200

    add max price validation

--- src/main/java/org/bookstore/dto/request/search/BookSearchParametersRequestDto.java
+++ src/main/java/org/bookstore/dto/request/search/BookSearchParametersRequestDto.java
@@ -1,12 +1,31 @@
 package org.bookstore.dto.request.search;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 
 public record BookSearchParametersRequestDto(
+
         String titlePart,
+
         String authorPart,
+
         String isbnPart,
+
+        @PositiveOrZero
         BigDecimal minPrice,
+
+        @Positive
         BigDecimal maxPrice,
-        String descriptionPart) {
+
+        String descriptionPart
+) {
+    @AssertTrue
+    public boolean isMaxPriceMoreThanMinPrice() {
+        if (minPrice == null || maxPrice == null) {
+            return true;
+        }
+        return maxPrice().compareTo(minPrice()) > 0;
+    }
 }
