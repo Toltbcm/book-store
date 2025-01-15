6fad3089bd3c6b9ce31066796f46ee9483433e2b
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-15 23:27:11 +0200

    add field isDeleted to user

--- src/main/java/org/bookstore/model/User.java
+++ src/main/java/org/bookstore/model/User.java
@@ -7,9 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 @Table(name = "users")
 public class User {
 
@@ -32,4 +36,7 @@ public class User {
 
     @Column(name = "shipping_address", nullable = false)
     private String shippingAddress;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 }
