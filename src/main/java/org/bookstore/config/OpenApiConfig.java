c365f076dcfd27d58cdd91bb3332c2fb2ce0a623
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-17 08:24:02 +0200

    change swagger config to annotations

--- src/main/java/org/bookstore/config/OpenApiConfig.java
+++ src/main/java/org/bookstore/config/OpenApiConfig.java
@@ -1,28 +1,23 @@
 package org.bookstore.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(name = "BearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer",
+        in = SecuritySchemeIn.HEADER, bearerFormat = "JWT")
+@SecurityScheme(name = "BasicAuth", type = SecuritySchemeType.HTTP, scheme = "basic",
+        in = SecuritySchemeIn.HEADER)
+@OpenAPIDefinition(
+        info = @Info(title = "Books API", description = "Book store app"),
+        security = {
+                @SecurityRequirement(name = "BearerAuth"),
+                @SecurityRequirement(name = "BasicAuth")}
+)
 public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI customOpenApi() {
-        return new OpenAPI().components(new Components()
-                        .addSecuritySchemes(
-                                "BearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT"))
-                        .addSecuritySchemes("BasicAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("basic")))
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth", "BasicAuth"));
-    }
 }
