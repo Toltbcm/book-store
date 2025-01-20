8021ec3e9ce27f3bd38cd7d52b831c7868cb4e7f
Anatolii <60557891+Toltbcm@users.noreply.github.com>
2025-01-20 15:30:22 +0200

    Role service, refactor (#11)
    * refactor
    
    * add role service
    
    * add validation to book dtos

--- src/main/java/org/bookstore/dto/request/CreateBookRequestDto.java
+++ src/main/java/org/bookstore/dto/request/CreateBookRequestDto.java
@@ -3,6 +3,7 @@ package org.bookstore.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 
 public record CreateBookRequestDto(
@@ -14,6 +15,7 @@ public record CreateBookRequestDto(
         String author,
 
         @NotBlank
+        @Size(min = 17, max = 17)
         String isbn,
 
         @NotNull
