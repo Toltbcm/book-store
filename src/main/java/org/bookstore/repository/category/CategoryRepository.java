d44635c1e5a81bd545fa24b6285bb28ebc00a747
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-20 13:58:49 +0200

    add repository category

---
+++ src/main/java/org/bookstore/repository/category/CategoryRepository.java
@@ -0,0 +1,7 @@
+package org.bookstore.repository.category;
+
+import org.bookstore.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
