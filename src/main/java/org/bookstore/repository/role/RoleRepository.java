f5f08660e14199aeca1211941bb9a83828925278
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-20 15:16:06 +0200

    add role service

--- src/main/java/org/bookstore/repository/role/RoleRepository.java
+++ src/main/java/org/bookstore/repository/role/RoleRepository.java
@@ -1,5 +1,6 @@
 package org.bookstore.repository.role;
 
+import java.util.Optional;
 import org.bookstore.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Role getByName(Role.RoleName name);
+    Optional<Role> getByName(Role.RoleName name);
 }
