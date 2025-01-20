df580200f429bd841c07a69be67faff30a27fa9e
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-20 20:37:54 +0200

    fix is_deleted

--- src/main/java/org/bookstore/model/Category.java
+++ src/main/java/org/bookstore/model/Category.java
@@ -12,7 +12,7 @@ import org.hibernate.annotations.SQLRestriction;
 
 @Data
 @Entity
-@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@SQLDelete(sql = "UPDATE categories SET is_deleted = true WHERE id = ?")
 @SQLRestriction("is_deleted = false")
 @Table(name = "categories")
 public class Category {
