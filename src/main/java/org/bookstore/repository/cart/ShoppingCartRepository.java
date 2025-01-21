3d2ea67b351726f682074233ffb07a293672d8cd
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-21 04:35:44 +0200

    add query for fetch full cart

--- src/main/java/org/bookstore/repository/cart/ShoppingCartRepository.java
+++ src/main/java/org/bookstore/repository/cart/ShoppingCartRepository.java
@@ -1,9 +1,16 @@
 package org.bookstore.repository.cart;
 
+import java.util.Optional;
 import org.bookstore.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+
+    @Query("FROM ShoppingCart c JOIN FETCH c.cartItems i JOIN FETCH i.book b "
+            + "JOIN FETCH c.user u WHERE u.email = :email")
+    Optional<ShoppingCart> fetchFullByUserEmail(@Param("email") String email);
 }
