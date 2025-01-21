570f9046f8d29acd3f3acc99e45a27a001a20c84
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-21 22:02:32 +0200

    remove redundant method

--- src/main/java/org/bookstore/mapper/MapperUtil.java
+++ src/main/java/org/bookstore/mapper/MapperUtil.java
@@ -1,11 +1,6 @@
 package org.bookstore.mapper;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import lombok.RequiredArgsConstructor;
-import org.bookstore.model.Category;
-import org.bookstore.service.CategoryService;
 import org.mapstruct.Named;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -15,17 +10,9 @@ import org.springframework.stereotype.Component;
 public class MapperUtil {
 
     private final PasswordEncoder passwordEncoder;
-    private final CategoryService categoryService;
 
     @Named("encodePassword")
     public String encodePassword(String password) {
         return passwordEncoder.encode(password);
-    }
-
-    @Named("setCategories")
-    public Set<Category> setCategories(List<Long> ids) {
-        Set<Category> categories = new HashSet<>();
-        ids.forEach(id -> categories.add(categoryService.getCategoryById(id)));
-        return categories;
     }
 }
