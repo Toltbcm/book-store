604415bde7f7316d5e864b948988eddc8d0848e0
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2025-01-21 10:16:04 +0200

    fix dto validation

--- src/main/java/org/bookstore/dto/request/UpdateCategoryRequestDto.java
+++ src/main/java/org/bookstore/dto/request/UpdateCategoryRequestDto.java
@@ -1,11 +1,11 @@
 package org.bookstore.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UpdateCategoryRequestDto(
 
-        @NotNull
+        @NotBlank
         @Size(max = 20)
         String name,
 
